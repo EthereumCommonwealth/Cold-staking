43a7abdee3e2b0aa815b15365fcd830ce791d0c7
joe <joe@joes-MacBook-Pro.local>
2018-06-12 09:56:14 -0700

    update test

--- test/ColdStaking.test.js
+++ test/ColdStaking.test.js
@@ -19,39 +19,43 @@ function ether(n) {
     return new BigNumber(web3.toWei(n, 'ether'));
 }
 
-contract('SampleCrowdsale', function (accounts) {
+contract('ColdStaking', function (accounts) {
 
     const [owner, wallet, investor] = accounts;
 
+    let _ColdStaking;
+
+
+    beforeEach(async function () {
+        _ColdStaking = await ColdStaking.new({from: owner});
+    });
+
 
     const _staking_threshold = new BigNumber(ether(0));
-
-    const _staking_pool = new BigNumber(ether(0));
 
     const _max_delay = new BigNumber(42000);
 
     const _round_interval = new BigNumber(200);
 
     it('should deploy', async function () {
-        const crowd = await ColdStaking.new();
 
-        const staking_threshold = await crowd.staking_threshold();
+        const staking_threshold = await _ColdStaking.staking_threshold();
 
         staking_threshold.should.be.bignumber.equal(_staking_threshold);
 
-        const staking_pool = await crowd.staking_pool();
+        const staking_pool = await _ColdStaking.staking_pool();
 
-        staking_pool.should.be.bignumber.equal(_staking_pool);
+        staking_pool.should.be.bignumber.equal(new BigNumber(ether(0)));
 
-        const reward = await crowd.reward();
+        const reward = await _ColdStaking.reward();
 
         reward.should.be.bignumber.equal(0);
 
-        const max_delay = await crowd.max_delay();
+        const max_delay = await _ColdStaking.max_delay();
 
         max_delay.should.be.bignumber.equal(_max_delay);
 
-        const round_interval = await crowd.round_interval();
+        const round_interval = await _ColdStaking.round_interval();
 
         round_interval.should.be.bignumber.equal(_round_interval);
 
@@ -60,13 +64,12 @@ contract('SampleCrowdsale', function (accounts) {
 
     it('should accept donation, increase balance and reward', async function () {
 
-        const crowd = await ColdStaking.new({from: owner});
 
-        const donation = await crowd.First_Stake_donation({value: ether(1)});
+        const donation = await _ColdStaking.First_Stake_donation({value: ether(1)});
 
-        const staking_pool = await crowd.staking_pool();
+        const staking_pool = await _ColdStaking.staking_pool();
 
-        const reward_ = await crowd.reward();
+        const reward_ = await _ColdStaking.reward();
 
         reward_.should.be.bignumber.equal(ether(1));
 
@@ -75,24 +78,22 @@ contract('SampleCrowdsale', function (accounts) {
 
     it('should start_staking', async function () {
 
-        const crowd = await ColdStaking.new({from: owner});
 
         const _value = ether(1);
 
-        const startStaking = await crowd.start_staking({value: _value, from: owner});
+        const startStaking = await _ColdStaking.start_staking({value: _value, from: owner});
 
-        const crowdBal = web3.eth.getBalance(crowd.address);
+        const crowdBal = await web3.eth.getBalance(_ColdStaking.address);
 
         crowdBal.should.be.bignumber.equal(_value);
 
         startStaking.logs[0].event.should.be.equal('StartStaking');
 
-        const staking_pool = await crowd.staking_pool();
+        const staking_pool = await _ColdStaking.staking_pool();
 
         staking_pool.should.be.bignumber.equal(_value);
 
-
-        const stakingInfo = await crowd.staker_info(owner);
+        const stakingInfo = await _ColdStaking.staker_info(owner);
 
         const [weight, init, stake_time, reward] = stakingInfo;
 
@@ -103,5 +104,35 @@ contract('SampleCrowdsale', function (accounts) {
 
     });
 
-})
+    it('fallback tx defaults to start_staking', async function () {
+
+
+        const _value = ether(1);
+
+
+        const startStaking = await _ColdStaking.sendTransaction({
+            from: owner,
+            value: _value,
+        });
+
+        const crowdBal = await web3.eth.getBalance(_ColdStaking.address);
+
+        crowdBal.should.be.bignumber.equal(_value);
+
+        startStaking.logs[0].event.should.be.equal('StartStaking');
+
+        const staking_pool = await _ColdStaking.staking_pool();
+
+        staking_pool.should.be.bignumber.equal(_value);
+
+        const stakingInfo = await _ColdStaking.staker_info(owner);
+
+        const [weight, init, stake_time, reward] = stakingInfo;
+
+        // stakingInfo.forEach(i => console.log(i.toNumber()))
+
+        weight.should.be.bignumber.equal(_value);
+    });
+
+});
 
